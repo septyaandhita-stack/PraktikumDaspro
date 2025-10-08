855067700715c760726bf6bab1ee590b73686c27
septyaandhita-stack <septyaandhita@gmail.com>
2025-10-08 17:20:20 +0700

    Percobaan 2

--- Praktikum05/PemilihanHari24.java
+++ Praktikum05/PemilihanHari24.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class pemilihanHari24 {
+public class PemilihanHari24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String dayName, dayType;
