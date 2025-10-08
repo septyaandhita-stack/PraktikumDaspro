dc00627b58cd123bccf38addaf2056dff899afbe
septyaandhita-stack <septyaandhita@gmail.com>
2025-10-08 11:15:38 +0700

    Percoban 1 Ternary

--- Praktikum05/CetakKRS24.java
+++ Praktikum05/CetakKRS24.java
@@ -2,17 +2,20 @@ import java.util.Scanner;
 
 public class CetakKRS24 {
     public static void main(String[] args) {
-    Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
 
-    System.out.println("--- Cetak KRS SIAKAD ---");
-    System.out.println("Apakah UKT sudah lunas? (true/false): ");
-    boolean uktLunas = sc.nextBoolean();
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.println("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
 
-    if (uktLunas) {
-        System.out.println("Pembayaran UKT terverivikasi");
-        System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");    
-    }else {
-        System.out.println("Registrasi ditolak. silahkanm lunasi UKT terlebih dahulu");
-    }
+        // if (uktLunas) {
+        //     System.out.println("Pembayaran UKT terverivikasi");
+        //     System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        // } else {
+        //     System.out.println("Registrasi ditolak. silahkanm lunasi UKT terlebih dahulu");
+        String hasil = uktLunas
+        ? "Pembayaran UKT terveerivikasi\nSilahkan cetak KRS dan minta tanda tangan DPA"
+        : "Refistrasi ditolak. Silahkan lunasi UKT terlebih dahulu";
+        System.out.println(hasil);
     }
 }
