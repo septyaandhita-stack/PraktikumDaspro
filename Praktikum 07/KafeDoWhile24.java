8a734ccb062e996f668093eba39266d6256894c7
septyaandhita-stack <septyaandhita@gmail.com>
2025-10-29 11:44:33 +0700

    test

---
+++ Praktikum 07/KafeDoWhile24.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class KafeDoWhile24 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int kopi = 12000;
+        int teh = 7000;
+        int roti = 2000;
+        
+    }
+
+}
