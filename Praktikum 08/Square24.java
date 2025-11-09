af68a7cf62e368cea103757ad07a89894054ce72
septyaandhita-stack <septyaandhita@gmail.com>
2025-11-10 04:34:31 +0700

    Square24

--- Praktikum 08/Square24.java
+++ Praktikum 08/Square24.java
@@ -7,7 +7,7 @@ public class Square24 {
 
         System.out.println("Masukkan nilai n: ");
         for (int iOuter=1; iOuter<=n; iOuter++) {
-            for (int i = 0; i <= n; i++) {
+            for (int i = 1; i <= n; i++) {
                 System.out.print("*");
         }
         System.out.println();
