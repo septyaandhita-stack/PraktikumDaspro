b0c763fda7620e4f82d130d3c2a700dd340f3fcc
septyaandhita-stack <septyaandhita@gmail.com>
2025-11-14 05:24:35 +0700

    Array nilai 23

--- Praktikum09/ArrayNilai23.java
+++ Praktikum09/ArrayNilai23.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ArrayNilai23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("test ");
 
         int[] nilaiAkhir = new int[10];
 
