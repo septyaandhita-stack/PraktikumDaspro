21bfec2db565a676a0d03b1d3796bce383fcc511
septyaandhita-stack <septyaandhita@gmail.com>
2025-11-14 08:37:44 +0700

    Search Nilai modifikasi

--- Praktikum09/SearchNilai23.java
+++ Praktikum09/SearchNilai23.java
@@ -5,18 +5,38 @@ import java.util.Scanner;
 public class SearchNilai23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arrNilai = { 80, 85, 78, 96, 90, 82, 86 };
-        int key = 90;
-        int hasil = 0;
 
-        for (int i = 0; i < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
-                hasil = 1;
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int n = sc.nextInt();
+
+        int[] arrNilai = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
+
+        int hasil = -1; 
+
+        for (int i = 0; i < n; i++) {
+            if (arrNilai[i] == key) {
+                hasil = i;
                 break;
             }
         }
+
         System.out.println();
-        System.out.println("Nilai "+key+" ketemu di indeks ke-"+hasil);
+
+        if (hasil != -1) {
+            // Tampilkan indeks +1 sesuai contoh output (nomor mahasiswa)
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
+        } else {
+            System.out.println("Nilai " + key + " tidak ditemukkan dalam array.");
+        }
+
         System.out.println();
     }
 }
