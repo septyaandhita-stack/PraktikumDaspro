565da768e2538ce4514a67a6a37bb1b5da26d2b2
septyaandhita-stack <septyaandhita@gmail.com>
2025-11-14 07:32:26 +0700

    Array rata nilai modifikasi

--- Praktikum09/ArrayRataNilai23.java
+++ Praktikum09/ArrayRataNilai23.java
@@ -5,22 +5,47 @@ import java.util.Scanner;
 public class ArrayRataNilai23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
+
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jumlahMhs = sc.nextInt();
+
+        int[] nilaiMhs = new int[jumlahMhs];
         double total = 0;
         double rata2;
 
-        for (int i = 0; i < nilaiMhs.length; i++) {
+        for (int i = 0; i < jumlahMhs; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         }
-        for (int i = 0; i < nilaiMhs.length; i++) {
-                    total += nilaiMhs[i];
 
-                }
-                rata2 = total /nilaiMhs.length;
-                System.out.println("Rata-rata nilai = " + rata2);
+        double totalLulus = 0, totalTidakLulus = 0;
+        int menghitungLulus = 0, menghitungTidakLulus = 0;
+        int nilaiTertinggi = nilaiMhs[0];
+        int nilaiTerendah = nilaiMhs[0];
+
+        for (int i = 0; i < jumlahMhs; i++) {
+
+        if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                menghitungLulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+                menghitungTidakLulus++;
+            }
+        
+            if (nilaiMhs[i] > nilaiTertinggi) {
+                nilaiTertinggi = nilaiMhs[i];
+        
+            } 
+            if (nilaiMhs[i] < nilaiTerendah) {
+                nilaiTerendah = nilaiMhs[i];
             }
 
-        }
-    
-    
+            }            
+
+        System.out.println("Rata-rata nilai lulus = " + (totalLulus / menghitungLulus));
+        System.out.println("Rata-rata nilai tidak lulus = " + (totalTidakLulus / menghitungTidakLulus));
+        System.out.println("Nilai tertinggi: " + nilaiTertinggi);
+        System.out.println("Nilai terendah: " + nilaiTerendah);
+    }
+}
