f9927a1e1a1e09797cc540a161df98e6527637bd
septyaandhita-stack <septyaandhita@gmail.com>
2025-11-19 10:37:35 +0700

    Bioskop modifikasi

--- Praktikum10/Bioskop23.java
+++ Praktikum10/Bioskop23.java
@@ -11,11 +11,33 @@ public class Bioskop23 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana"; //modif 1
 
         System.out.printf("%s \t %s\n", penonton [0][0], penonton[0][1]);
         System.out.printf("%s \t %s\n", penonton [1][0], penonton[1][1]);
         System.out.printf("%s \t %s\n", penonton [2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton [3][0], penonton[3][1]);
-                
+
+        System.out.println(penonton.length);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + String.join(",", penonton[i]));
+        }
+
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris : " + barisPenonton.length);
+        }
+
+        System.out.println("Penonton pada baris ke-3: ");
+
+        for (String i : penonton[2]) {
+            System.out.println(i);
+        }
+        
+        //System.out.println(penonton[0].length);
+        //System.out.println(penonton[1].length);
+        //System.out.println(penonton[2].length);
+        //System.out.println(penonton[3].length);   
+        
+        
     }
 }
