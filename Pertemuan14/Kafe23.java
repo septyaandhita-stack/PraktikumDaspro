cabc4bc75588dedb3e57cfbd7b0a9fe3b6828a87
septyaandhita-stack <septyaandhita@gmail.com>
2025-12-02 10:33:10 +0700

    Kafe 23

---
+++ Pertemuan14/Kafe23.java
@@ -0,0 +1,21 @@
+package Pertemuan14;
+import java.nio.file.ProviderMismatchException;
+import java.util.Scanner;
+public class Kafe23 {
+    public static void Menu() {
+        System.out.println("===== MENU RESTO CAFE =====");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappucino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,000");
+        System.out.println("4. Teh Tarik - Rp 12,000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
+        System.out.println("===========================");
+        System.out.println("Silahkan pilih menu yang anda inginkan");
+
+        }
+    
+     public static void main(String[] args) {
+            Menu();
+        }
+}
